ee65bf7d12cca1d776e568702e8c2d58c52d942b
mateuszbabski <babskimateusz@gmail.com>
2023-03-12 23:26:21 +0100

    DBContext registered into infrastructure layer

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Context
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get;  set; }
     }
 }
--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Infrastructure.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-        {         
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IApplicationDbContext>(options => options.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            
 
             return services;
         }
