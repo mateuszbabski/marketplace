0b515bd140735df4ffdee44cc7b3f28a186147e7
mateuszbabski <babskimateusz@gmail.com>
2023-03-14 12:05:17 +0100

    Authentication controller sketch

---
+++ WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿//using Application.Authentication;
+//using Application.Authentication.Services;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace WebAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AuthenticationController : ControllerBase
+//    {
+//        private readonly IAuthenticationService _authenticationService;
+
+//        public AuthenticationController(IAuthenticationService authenticationService)
+//        {
+//            _authenticationService = authenticationService;
+//        }
+
+//        [HttpPost("authenticate")]
+//        public async Task<IActionResult> AuthenticateAsync([FromBody] LoginRequest request)
+//        {
+//            return Ok(await _authenticationService.Login(request));
+//        }
+
+//        [HttpPost("register-customer")]
+//        public async Task<IActionResult> AuthenticateAsync([FromBody] RegisterCustomerRequest request)
+//        {
+//            return Ok(await _authenticationService.Register(request));
+//        }
+//    }
+//}
