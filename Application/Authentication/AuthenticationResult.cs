1611c54d26e68a9b37767ad687dd7601db1e5aab
mateuszbabski <babskimateusz@gmail.com>
2023-03-14 12:04:29 +0100

    Authresult, LoginRequest created

---
+++ Application/Authentication/AuthenticationResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Authentication
+{
+    public record AuthenticationResult(
+         Guid Id,
+         string Token);
+    
+}
