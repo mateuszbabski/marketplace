1f8c1222a3c07768bbf4e7dbfc8a017dafca154e
mateuszbabski <babskimateusz@gmail.com>
2023-03-15 21:15:52 +0100

    Register customer validation

--- Application/Authentication/LoginRequest.cs
+++ Application/Authentication/LoginRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace Application.Authentication
 {
     public class LoginRequest
     {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
     }
 }
--- Application/Authentication/RegisterCustomerRequest.cs
+++ Application/Authentication/RegisterCustomerRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,18 @@ namespace Application.Authentication
 {
     public class RegisterCustomerRequest
     {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string Name { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public string TelephoneNumber { get; set; } = null!;
-        public string Country { get; set; } = null!;
-        public string City { get; set; } = null!;
-        public string Street { get; set; } = null!;
-        public string PostalCode { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
     }
 }
