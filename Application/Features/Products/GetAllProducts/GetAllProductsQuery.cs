3ea6aaad3f17ce01b06a01e9baa135a2bca161ff
mateuszbabski <babskimateusz@gmail.com>
2023-03-22 21:24:44 +0100

    Get all products query

---
+++ Application/Features/Products/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Shop.Entities.Products;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.GetAllProducts
+{
+    public class GetAllProductsQuery : IRequest<List<ProductDto>>
+    {
+    }
+}
---
+++ Application/Features/Products/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Shop.Entities.Products;
+using Domain.Shop.Entities.Products.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.GetAllProducts
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<ProductDto>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetAllProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<List<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetAllProducts();
+            if (products == null)
+            {
+                throw new Exception("Products not found");
+            }            
+
+            
+            var productList = new List<ProductDto>();
+            foreach (var product in products)
+            {
+                var productDto = new ProductDto()
+                {
+                    Id = product.Id,
+                    ProductName = product.ProductName,
+                    ProductDescription = product.ProductDescription,
+                    Unit = product.Unit,
+                    IsAvailable = product.IsAvailable,
+                    ShopId = product.ShopId,
+                    Price = product.Price
+                };
+
+                productList.Add(productDto);                
+            }            
+            
+            return productList;
+        }
+    }
+}
