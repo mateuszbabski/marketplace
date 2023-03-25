a15da3feb6cd86d7be488cf7d79dba201113c64a
mateuszbabski <babskimateusz@gmail.com>
2023-03-25 20:38:41 +0100

    CurrentUserService httpcontext

---
+++ Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        ClaimsPrincipal User { get; }
+        Guid UserId { get; }
+        
+    }
+}
---
+++ WebAPI/Services/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace WebAPI.Services
+{   
+
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CurrentUserService(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public ClaimsPrincipal User => _httpContext.HttpContext?.User;
+
+        public Guid UserId => Guid.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+    }
+}
