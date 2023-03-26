01b5bf343d3f6f7c38df144e40089cdcf4be5491
mateuszbabski <babskimateusz@gmail.com>
2023-03-26 17:46:15 +0200

    Common response and validation exception

---
+++ Application/Common/Responses/BaseResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Responses
+{
+    public class BaseResponse<T>
+    {
+        public BaseResponse()
+        {
+
+        }
+
+        public BaseResponse(T data, string message = null)
+        {
+            Succeeded = true;
+            Message = message;
+            Data = data;
+        }
+        public BaseResponse(string message)
+        {
+            Succeeded = false;
+            Message = message;
+        }
+
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+    }
+}
