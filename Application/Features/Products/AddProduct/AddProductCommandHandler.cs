3c3cdd8488289ad60be8d8471bba22b1403c6ae8
mateuszbabski <babskimateusz@gmail.com>
2023-03-29 21:58:36 +0200

    Add product handler refactored

--- Application/Features/Products/AddProduct/AddProductCommandHandler.cs
+++ Application/Features/Products/AddProduct/AddProductCommandHandler.cs
@@ -2,6 +2,7 @@
 using Domain.Shared.ValueObjects;
 using Domain.Shops;
 using Domain.Shops.Entities.Products.Repositories;
+using Domain.Shops.Repositories;
 using MediatR;
 
 namespace Application.Features.Products.AddProduct
@@ -9,12 +10,14 @@ namespace Application.Features.Products.AddProduct
     public class AddProductCommandHandler : IRequestHandler<AddProductCommand, Guid>
     {
         private readonly IProductRepository _productRepository;
+        private readonly IShopRepository _shopRepository;
         private readonly ICurrentUserService _userService;
 
-        public AddProductCommandHandler(IProductRepository productRepository,
+        public AddProductCommandHandler(IProductRepository productRepository, IShopRepository shopRepository,
                                         ICurrentUserService userService)
         {
             _productRepository = productRepository;
+            _shopRepository = shopRepository;
             _userService = userService;
         }
 
@@ -22,6 +25,8 @@ namespace Application.Features.Products.AddProduct
         public async Task<Guid> Handle(AddProductCommand request, CancellationToken cancellationToken)
         {
             var shopId = _userService.UserId;
+            var shop = await _shopRepository.GetShopById(shopId);
+
             var validator = new AddProductCommandValidator();
             var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
@@ -32,7 +37,7 @@ namespace Application.Features.Products.AddProduct
             //TODO validation exception
             var price = MoneyValue.Of(request.Amount, request.Currency);
             
-            var product = Shop.AddProduct(Guid.NewGuid(),
+            var product = shop.AddProduct(Guid.NewGuid(),
                                                  request.ProductName,
                                                  request.ProductDescription,
                                                  price,
