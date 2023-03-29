e0ed26c328b8fd3fecbe24086163b9d3b6bed889
mateuszbabski <babskimateusz@gmail.com>
2023-03-29 21:57:55 +0200

    Entity abstraction + business rule exception

---
+++ Domain/Shared/Abstractions/Entity.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Shared.Abstractions
+{
+    public abstract class Entity
+    {
+        protected static void CheckRule(IBusinessRule rule)
+        {
+            if (rule.IsBroken())
+            {
+                throw new BusinessRuleValidationException(rule);
+            }
+        }
+    }
+}
---
+++ Domain/Shared/Exceptions/BusinessRuleValidationException.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Shared.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Shared.Exceptions
+{
+    public class BusinessRuleValidationException : Exception
+    {
+        public IBusinessRule BrokenRule { get; }
+        public string Details { get;}
+
+        public BusinessRuleValidationException(IBusinessRule brokenRule) : base(brokenRule.Message)
+        {
+            BrokenRule = brokenRule;
+            this.Details = brokenRule.Message;
+        }
+
+        public override string ToString()
+        {
+            return $"{BrokenRule.GetType().FullName}: {BrokenRule.Message}";
+        }
+    }
+}
