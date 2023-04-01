76d0c49c0491bc34794b91c036704e2fe0aed733
mateuszbabski <babskimateusz@gmail.com>
2023-04-01 20:58:22 +0200

    Shopping Cart repository

---
+++ Domain/Customers/Entities/ShoppingCarts/Repositories/IShoppingCartRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Customers.Entities.ShoppingCarts;
+using Domain.Customers.Entities.ShoppingCarts.ValueObjects;
+using Domain.Customers.ValueObjects;
+
+namespace Domain.Customers.Entities.ShoppingCarts.Repositories
+{
+    public interface IShoppingCartRepository
+    {
+        Task<ShoppingCart> Create(ShoppingCart shoppingCart);
+        Task<ShoppingCart> GetShoppingCartByCustomerId(CustomerId customerId);
+        Task<ShoppingCart> GetShoppingCartById(ShoppingCartId id);
+        Task Update(ShoppingCart shoppingCart);
+    }
+}
---
+++ Infrastructure/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Customers.Entities.ShoppingCarts;
+using Domain.Customers.Entities.ShoppingCarts.Repositories;
+using Domain.Customers.Entities.ShoppingCarts.ValueObjects;
+using Domain.Customers.ValueObjects;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    internal sealed class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public ShoppingCartRepository(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ShoppingCart> Create(ShoppingCart shoppingCart)
+        {
+            await _dbContext.ShoppingCarts.AddAsync(shoppingCart);
+            await _dbContext.SaveChangesAsync();
+
+            return shoppingCart;
+        }
+
+        public async Task Update(ShoppingCart shoppingCart)
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(ShoppingCart shoppingCart)
+        {
+            _dbContext.ShoppingCarts.Remove(shoppingCart);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ShoppingCart> GetShoppingCartById(ShoppingCartId id)
+        {
+            return await _dbContext.ShoppingCarts.Include(x => x.Items)
+                                                 .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<ShoppingCart> GetShoppingCartByCustomerId(CustomerId customerId)
+        {
+            return await _dbContext.ShoppingCarts.Include(x => x.Items)
+                                                 .FirstOrDefaultAsync(x => x.CustomerId == customerId);
+        }
+    }
+}
