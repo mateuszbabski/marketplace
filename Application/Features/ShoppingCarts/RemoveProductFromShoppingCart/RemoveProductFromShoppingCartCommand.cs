e71d4fc1114dc904fb52e4d353876831fc10d0de
mateuszbabski <babskimateusz@gmail.com>
2023-04-10 18:41:54 +0200

    Remove product from cart feature

---
+++ Application/Features/ShoppingCarts/RemoveProductFromShoppingCart/RemoveProductFromShoppingCartCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ShoppingCarts.RemoveProductFromShoppingCart
+{
+    public class RemoveProductFromShoppingCartCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Application/Features/ShoppingCarts/RemoveProductFromShoppingCart/RemoveProductFromShoppingCartCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Interfaces;
+using Domain.Customers.Entities.ShoppingCarts.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ShoppingCarts.RemoveProductFromShoppingCart
+{
+    public class RemoveProductFromShoppingCartCommandHandler : IRequestHandler<RemoveProductFromShoppingCartCommand, Unit>
+    {
+        private readonly ICurrentUserService _userService;
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public RemoveProductFromShoppingCartCommandHandler(ICurrentUserService userService, IShoppingCartRepository shoppingCartRepository)
+        {
+            _userService = userService;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+        public async Task<Unit> Handle(RemoveProductFromShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            var customerId = _userService.UserId;
+            var shoppingCart = await _shoppingCartRepository.GetShoppingCartByCustomerId(customerId);
+
+            if (shoppingCart == null)
+            {
+                throw new Exception("Cart not found.");
+            }
+
+            shoppingCart.RemoveItemFromCart(request.Id);
+
+            await _shoppingCartRepository.RemoveItem(shoppingCart, request.Id);
+
+            return Unit.Value;
+        }
+    }
+}
