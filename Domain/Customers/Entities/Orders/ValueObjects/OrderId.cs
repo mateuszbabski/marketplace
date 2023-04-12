f8b25cb63d5d7c47f9674b10b3652be87f835408
mateuszbabski <babskimateusz@gmail.com>
2023-04-12 19:53:45 +0200

    Order Value Objects

---
+++ Domain/Customers/Entities/Orders/ValueObjects/OrderId.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Customers.Entities.Orders.Exceptions;
+using Domain.Customers.Entities.ShoppingCarts.Exceptions;
+using Domain.Customers.Entities.ShoppingCarts.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customers.Entities.Orders.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+
+        public OrderId(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyOrderIdException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(OrderId id) => id.Value;
+        public static implicit operator OrderId(Guid value) => new(value);
+    }
+}
---
+++ Domain/Customers/Entities/Orders/ValueObjects/OrderItemId.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Customers.Entities.Orders.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customers.Entities.Orders.ValueObjects
+{
+    public record OrderItemId
+    {
+        public Guid Value { get; }
+
+        public OrderItemId(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyOrderItemIdException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(OrderItemId id) => id.Value;
+        public static implicit operator OrderItemId(Guid value) => new(value);
+    }
+}
