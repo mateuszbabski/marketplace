7b7638ef0229e8cc86bc66d72a6325c0fccde156
mateuszbabski <babskimateusz@gmail.com>
2023-04-12 19:53:37 +0200

    Order Exceptions

---
+++ Domain/Customers/Entities/Orders/Exceptions/EmptyOrderIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customers.Entities.Orders.Exceptions
+{
+    public class EmptyOrderIdException : Exception
+    {
+        public EmptyOrderIdException() : base(message: "OrderId can not be empty.")
+        {
+        }
+    }
+}
---
+++ Domain/Customers/Entities/Orders/Exceptions/EmptyOrderItemIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customers.Entities.Orders.Exceptions
+{
+    public class EmptyOrderItemIdException : Exception
+    {
+        public EmptyOrderItemIdException() : base(message: "OrderItemId can not be empty.")
+        {
+        }
+    }
+}
