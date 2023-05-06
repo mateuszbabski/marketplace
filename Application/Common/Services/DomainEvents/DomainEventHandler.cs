12c896c94c7a9aae54bd596a78358e1a7190eb20
mateuszbabski <babskimateusz@gmail.com>
2023-05-06 22:49:25 +0200

    DomainEventHandler Application layer init

---
+++ Application/Common/Services/DomainEvents/DomainEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Shared.Abstractions;
+using MediatR;
+
+namespace Application.Common.Services.DomainEvents
+{
+    public class DomainEventHandler<T> : IDomainEventHandler<T> where T : IDomainEvent
+    {
+        public T DomainEvent { get; }
+
+        public DomainEventHandler(T domainEvent)
+        {
+            this.DomainEvent = domainEvent;
+        }
+    }
+}
---
+++ Application/Common/Services/DomainEvents/IDomainEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Shared.Abstractions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Services.DomainEvents
+{
+    public interface IDomainEventHandler<out TEventType> : IDomainEventHandler
+    {
+        TEventType DomainEvent { get; }
+    }
+    public interface IDomainEventHandler : INotification
+    {
+    }
+}
