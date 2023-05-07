f996bd399a8f288d767eb77327922394cf8379fe
mateuszbabski <babskimateusz@gmail.com>
2023-05-07 15:49:46 +0200

    Invoice controller init

---
+++ WebAPI/Controllers/InvoiceController.cs
@@ -0,0 +1,40 @@
+﻿//using MediatR;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Application.Features.Invoices;
+
+//namespace WebAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class InvoiceController : ControllerBase
+//    {
+//        private readonly Mediator _mediator;
+
+//        public InvoiceController(Mediator mediator)
+//        {
+//            _mediator = mediator;
+//        }
+
+//        [Authorize(Roles = "customer")]
+//        [HttpGet("GetInvoice")]
+//        public async Task<ActionResult<InvoiceDetailsDto>> GetInvoiceByIdForCustomer(Guid id)
+//        {
+//            var invoice = await _mediator.Send(new GetInvoiceByIdQuery()
+//            {
+//                Id = id
+//            });
+
+//            return Ok(invoice);
+//        }
+
+//        [Authorize(Roles = "customer")]
+//        [HttpGet("GetAllCustomerInvoices")]
+//        public async Task<ActionResult<IEnumerable<InvoiceDto>>> GetAllInvoicesForCustomer()
+//        {
+//            var invoices = await _mediator.Send(new GetInvoicesQuery());
+
+//            return Ok(invoices);
+//        }
+//    }
+//}
---
+++ WebAPI/Controllers/ShopInvoiceController.cs
@@ -0,0 +1,40 @@
+﻿//using MediatR;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Application.Features.ShopInvoices;
+
+//namespace WebAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ShopInvoiceController : ControllerBase
+//    {
+//        private readonly Mediator _mediator;
+
+//        public ShopInvoiceController(Mediator mediator)
+//        {
+//            _mediator = mediator;
+//        }
+
+//        [Authorize(Roles = "shop")]
+//        [HttpGet("GetShopInvoice")]
+//        public async Task<ActionResult<ShopInvoiceDetailsDto>> GetInvoiceByIdForShop(Guid id)
+//        {
+//            var invoice = await _mediator.Send(new GetShopInvoiceByIdQuery()
+//            {
+//                Id = id
+//            });
+
+//            return Ok(invoice);
+//        }
+
+//        [Authorize(Roles = "shop")]
+//        [HttpGet("GetAllShopInvoices")]
+//        public async Task<ActionResult<IEnumerable<ShopInvoiceDto>>> GetAllInvoicesForShop()
+//        {
+//            var invoices = await _mediator.Send(new GetShopInvoicesQuery());
+
+//            return Ok(invoices);
+//        }
+//    }
+//}
