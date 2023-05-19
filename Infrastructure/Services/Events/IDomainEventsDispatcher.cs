dd4a83af2ca94e9039f8408991812d3c98760b7e
mateuszbabski <babskimateusz@gmail.com>
2023-05-19 21:25:27 +0200

    Dispatcher log

--- Infrastructure/Services/Events/IDomainEventsDispatcher.cs
+++ Infrastructure/Services/Events/IDomainEventsDispatcher.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Services.Events
+﻿namespace Infrastructure.Services.Events
 {
     public interface IDomainEventsDispatcher
     {
