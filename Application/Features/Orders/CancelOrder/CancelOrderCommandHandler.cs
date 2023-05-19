b3bdafe45db0be1b3d81a3d384ff3ecd7506acb2
mateuszbabski <babskimateusz@gmail.com>
2023-05-19 21:22:41 +0200

    Cancel Order refactored

--- Application/Features/Orders/CancelOrder/CancelOrderCommandHandler.cs
+++ Application/Features/Orders/CancelOrder/CancelOrderCommandHandler.cs
@@ -18,20 +18,14 @@ namespace Application.Features.Orders.CancelOrder
     {
         private readonly ICurrentUserService _userService;
         private readonly ICustomerRepository _customerRepository;
-        private readonly IShopOrderRepository _shopOrderRepository;
-        private readonly IInvoiceRepository _invoiceRepository;
         private readonly IUnitOfWork _unitOfWork;
 
         public CancelOrderCommandHandler(ICurrentUserService userService,
                                        ICustomerRepository customerRepository,
-                                       IShopOrderRepository shopOrderRepository,
-                                       IInvoiceRepository invoiceRepository,
                                        IUnitOfWork unitOfWork)
         {
             _userService = userService;
             _customerRepository = customerRepository;
-            _shopOrderRepository = shopOrderRepository;
-            _invoiceRepository = invoiceRepository;
             _unitOfWork = unitOfWork;
         }
         public async Task Handle(CancelOrderCommand request, CancellationToken cancellationToken)
@@ -41,28 +35,7 @@ namespace Application.Features.Orders.CancelOrder
 
             customer.CancelOrder(request.Id);
 
-            //await CancelRelatedShopOrders(request.Id);
-
-            //await CancelAllInvoices(request.Id);
-
             await _unitOfWork.CommitAsync();
         }
-
-        //private async Task CancelRelatedShopOrders(OrderId orderId)
-        //{
-        //    var shopOrderList = await _shopOrderRepository.GetShopOrdersByOrderId(orderId);
-
-        //    foreach (var shopOrder in shopOrderList)
-        //    {
-        //        shopOrder.CancelOrder();
-        //    }
-        //}
-
-        //private async Task CancelAllInvoices(OrderId orderId)
-        //{
-        //    var invoice = await _invoiceRepository.GetInvoiceByOrderId(orderId);
-
-        //    invoice.CancelInvoice();
-        //}
     }
 }
--- Application/Features/Orders/CancelOrder/OrderCancelledDomainEventHandler.cs
+++ Application/Features/Orders/CancelOrder/OrderCancelledDomainEventHandler.cs
@@ -1,15 +1,8 @@
-﻿using Domain.Customers.Entities.Orders;
-using Domain.Customers.Entities.Orders.Events;
-using Domain.Customers.Entities.Orders.ValueObjects;
+﻿using Domain.Customers.Entities.Orders.Events;
 using Domain.Invoices.Repositories;
 using Domain.Shops.Entities.ShopOrders.Repositories;
 using MediatR;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Orders.CancelOrder
 {
@@ -32,12 +25,10 @@ namespace Application.Features.Orders.CancelOrder
             {
                 shopOrder.CancelOrder();
             }
-            Log.Information("shop orders cancelled");
 
             var invoice = await _invoiceRepository.GetInvoiceByOrderId(notification.OrderId);
 
             invoice.CancelInvoice();
-            Log.Information("invoices cancelled");
         }
     }
 }
