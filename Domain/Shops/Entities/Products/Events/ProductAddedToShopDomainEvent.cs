c6c98369775ad330f3c9580c0580200fb5103b1c
mateuszbabski <babskimateusz@gmail.com>
2023-05-19 21:24:56 +0200

    Products events

---
+++ Domain/Shops/Entities/Products/Events/ProductAddedToShopDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Shared.Abstractions;
+
+namespace Domain.Shops.Entities.Products.Events
+{
+    public sealed record ProductAddedToShopDomainEvent(Product Product) : IDomainEvent
+    {
+    }
+}
---
+++ Domain/Shops/Entities/Products/Events/ProductRemovedFromShopDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Shared.Abstractions;
+
+namespace Domain.Shops.Entities.Products.Events
+{
+    public sealed record ProductRemovedFromShopDomainEvent(Product Product) : IDomainEvent
+    {
+    }
+}
