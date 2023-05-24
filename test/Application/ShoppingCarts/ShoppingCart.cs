3246d6f2d75f48bbd774c431367caa374cae6105
mateuszbabski <babskimateusz@gmail.com>
2023-05-24 16:06:51 +0200

    shopping cart mock for test

---
+++ test/Application/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Customers;
+using Domain.Customers.Entities.ShoppingCarts;
+using UnitTest.Application.Products;
+
+namespace UnitTest.Application.ShoppingCarts
+{
+    public class ShoppingCartMock
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCart ShoppingCart
+        {
+            get
+            {
+                return _shoppingCart;
+            }
+        }
+
+        public ShoppingCartMock(Customer customer)
+        {
+            var cart = ShoppingCart.CreateShoppingCart(customer.Id, "USD");
+
+            var products = new ProductList();
+
+            cart.AddProductToShoppingCart(products.Products[0], 1, products.Products[0].Price.Amount);
+            cart.AddProductToShoppingCart(products.Products[1], 1, products.Products[1].Price.Amount);
+
+            _shoppingCart = cart;
+        }
+    }
+}
